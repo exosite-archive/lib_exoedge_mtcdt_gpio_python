b4a98c5f55ec1bce6df9b8db70861323ef09f459
Will Charlton <willcharlton@exosite.com>
2018-12-06 13:24:55 -0600

    Updated for 18.11.17 release of ExoEdge.

--- exoedge_mtcdt_gpio/__init__.py
+++ exoedge_mtcdt_gpio/__init__.py
@@ -6,102 +6,34 @@ import sys
 import os
 import logging
 import time
-from exoedge.sources import AsyncSource
+from exoedge.sources import ExoEdgeSource
+from exoedge import logger
+from shownames import SHOWNAMES
 
-logging.getLogger('MTCDT_GPIO')
-logging.basicConfig(level=logging.DEBUG)
+LOG = logger.getLogger(__name__)
+LOG.setLevel(logging.DEBUG)
 
-SHOWNAMES = [
-    "ap1/product-id",
-    "ap1/adc0",
-    "ap1/adc1",
-    "ap1/adc2",
-    "ap1/din0",
-    "ap1/din1",
-    "ap1/din2",
-    "ap1/din3",
-    "ap1/led1",
-    "ap1/led2",
-    "ap1/device-id",
-    "ap1/vendor-id",
-    "ap1/dout0",
-    "ap1/dout1",
-    "ap1/dout2",
-    "ap1/dout3",
-    "ap1/reset",
-    "ap1/dout-enable",
-    "ap1/hw-version",
-    "capability/adc",
-    "capability/din",
-    "capability/gps",
-    "capability/dout",
-    "capability/lora",
-    "capability/wifi",
-    "capability/bluetooth",
-    "device-id",
-    "eth-reset",
-    "gpiob/product-id",
-    "gpiob/adc0",
-    "gpiob/adc1",
-    "gpiob/adc2",
-    "gpiob/din0",
-    "gpiob/din1",
-    "gpiob/din2",
-    "gpiob/din3",
-    "gpiob/led1",
-    "gpiob/led2",
-    "gpiob/device-id",
-    "gpiob/vendor-id",
-    "gpiob/dout0",
-    "gpiob/dout1",
-    "gpiob/dout2",
-    "gpiob/dout3",
-    "gpiob/reset",
-    "gpiob/dout-enable",
-    "gpiob/hw-version",
-    "has-radio",
-    "hw-version",
-    "imei",
-    "led-a",
-    "led-b",
-    "led-c",
-    "led-cd",
-    "led-d",
-    "led-sig1",
-    "led-sig2",
-    "led-sig3",
-    "led-status",
-    "mac-eth",
-    "product-id",
-    "reset",
-    "reset-monitor",
-    "reset-monitor-intervals",
-    "uuid",
-    "vendor-id",
-]
 
-class MTCDTGPIOExoEdgeSource(AsyncSource):
+class MTCDTGPIOExoEdgeSource(ExoEdgeSource):
     """ Exoedge GPIO source for the Multitech Conduit."""
-
     def show(self, *args, **kwargs):
         showname = kwargs.get('showname')
         if showname not in SHOWNAMES:
             return "Showname not supported: {}".format(showname)
         with open(os.path.join('/sys/devices/platform/mts-io', showname), 'r') as theshow:
+            try:
+                return int(theshow.read().strip())
+            except:
+                pass
             return theshow.read().strip()
 
     def run(self):
+
         while not self.is_stopped():
             time.sleep(0.1)
         logging.critical("{} HAS BEEN STOPPED.".format(self.name))
 
-# set up borg instance of async source
-src = MTCDTGPIOExoEdgeSource().get_async_source()
-
-# get access to the current module
-this_module = sys.modules[__name__]
-
-# register all desired class methods to module
-setattr(this_module, 'show', src.show)
+# register instance method with module so "classic" ExoEdge method can be used.
+setattr(sys.modules[__name__], 'show', MTCDTGPIOExoEdgeSource().get_source().show)
 
 logging.critical("current module methods: {}".format(dir(sys.modules[__name__])))
--- setup.py
+++ setup.py
@@ -14,9 +14,15 @@ try:
 except:
     README = 'README Not Found'
 
+
+version = {}
+with open("exoedge_mtcdt_gpio/__version__.py") as fp:
+    exec(fp.read(), version) # pylint: disable=W0122
+    assert version.get('__version__'), "Unable to parse version from exoedge_modbus/__version__.py."
+
 setup(
     name="exoedge_mtcdt_gpio",
-    version="18.08.10",
+    version=version['__version__'],
     author="Exosite LLC",
     author_email="support@exosite.com",
     description="An ExoEdge source for interfacing with GPIO on the Multitech Conduit device.",
